66e092696b9aff395f9f0855f26ff20927e9984d
Dhron Joshi <dhron.joshi@uwaterloo.ca>
2016-10-15 07:04:52 -0400

    starting the pebble src and the 'idea' for how the app works

---
+++ pebble src/index.js
@@ -0,0 +1,11 @@
+var rocky = require('rocky');
+
+//something we execute every time we draw
+//we want to get the location data, and based on some route that
+//we choose, display a 'compass' that represents the direction i'm facing
+
+//update the screen every time i change direction based on my phone's GPS
+//or Pebble's GPS
+rocky.on('draw', function(event){
+	
+});
